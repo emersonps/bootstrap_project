ea94c0c6f4eb41611d8605ff0c3bb20446d8693a
EMERSON\Emerson <emersonline2007@gmail.com>
2017-04-20 03:07:48 -0300

    Estilização de Conteúdo da Página, criação do Slider, âncoras, efeito scrool com JS, aplicação de efeito Sata-Spy (Bootstrap JS)

--- js/main.js
+++ js/main.js
@@ -1,35 +1,19 @@
-$(function(){
-	//equivalente ao load de nossa página
-	$("#modal01").modal({backdrop:false, keyboard:false}); //remover a cor do fundo e impedir o uso de ESC
-	
-	$("#modal01").modal("show"); //mostrar o modal ao abrir a página
-	
-	
-	setTimeout(function(){
-		$("#modal01").modal("hide"); //fechar o modal automaticamente
-	}, 3000);
+$(document).on('click', 'a[href^="#"]', function(e) { 
+// target element id 
+var id = $(this).attr('href'); 
 
-	$(".ttp").tooltip({
-		animation:false,
-		delay: {show : 1000, hide: 5000},
-		title: "Titulo Padrão",
-		triger: 'click'
-	});
+// target element 
+var $id = $(id); 
+if ($id.length === 0) { 
+return; 
+} 
 
-	$(".ppv").popover({
-		title: "Titulo do popover",
-		trigger: "hover focus",
-		delay: {show : 1000, hide: 5000},
-	});
+// prevent standard hash navigation (avoid blinking in IE) 
+e.preventDefault(); 
 
-	$(".btnjs").button();
+// top position relative to the document 
+var pos = $(id).offset().top; 
 
-	$("#troca-estado").click(function(){
-		var btn = $(this);
-		btn.button("loading");
-		
-		setTimeout(function(){
-			btn.button("reset");
-		}, 3000);
-	});
+// animated top scrolling 
+$('body, html').animate({scrollTop: pos}); 
 });
