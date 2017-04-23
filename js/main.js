96c40b5da698f13aa1cb5ebdbf7ba62e2d6436da
EMERSON\Emerson <emersonline2007@gmail.com>
2017-04-23 04:22:23 -0300

    Criação da área Portfolio, Quem somos, Fale conosco e rodapé.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Script para suaviação de rolagem da página
 $(document).on('click', 'a[href^="#"]', function(e) { 
 // target element id 
 var id = $(this).attr('href'); 
@@ -17,3 +18,5 @@ var pos = $(id).offset().top;
 // animated top scrolling 
 $('body, html').animate({scrollTop: pos}); 
 });
+
+
