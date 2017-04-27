4d897a4d840771c408b2eb7eae7c1826488d5555
EMERSON\Emerson <emersonline2007@gmail.com>
2017-04-27 01:09:37 -0300

    Alterações na Tela de Login, Término do Painel, Instalação e uso de Plugins (JS Contagem de caractere de campo), Aplicação de Temas.

--- js/main.js
+++ js/main.js
@@ -19,4 +19,17 @@ var pos = $(id).offset().top;
 $('body, html').animate({scrollTop: pos}); 
 });
 
+//instanciando o maxlenght do Pluggin Baixado no site https://codeload.github.com/mimo84/bootstrap-maxlength/legacy.zip/master
+//1º Baixa o Plugin desejado - 
+//2º Copia o arquivo do plugin desejado para a pasta JS (bootstrap-maxlength.jquery.json)
+//3º Importa o pluggin na página que deseja usar o pluggin no componente
+//4º No main.js, faz o script abaixo: ou copia do mesmo site: documentação:
+
+	$('input[maxlength]').maxlength({
+	    alwaysShow: true,
+	    threshold: 10,
+	    warningClass: "label label-success",
+	    limitReachedClass: "label label-danger"
+	});
+
 
